52a37c1a4303f6e943036f2af02fe3264c3993a9
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2018-09-18 13:00:22 -0400

    Serve a (trivial) website as a Tor Onion service
    This commit intentionally omits keys.  I need to determine how we will
    manage these keys.

--- ops/s4.nix
+++ ops/s4.nix
@@ -1,9 +1,14 @@
 # Describe the software to run on the infrastructure described by s4-ec2.nix.
 {
   network.description = "Zcash server";
+
   zcashnode =
-  { config, pkgs, ... }:
+  { lib, pkgs, ... }:
   let zcash = pkgs.callPackage ./zcash/default.nix { };
+      s4signupwebsite = pkgs.callPackage ./s4signupwebsite.nix { };
+      torControlPort = 9051;
+      websiteOnion2Dir = "/run/onion/v2/signup-website";
+      websiteOnion3Dir = "/run/onion/v3/signup-website";
   in
   # Allow the two Zcash protocol ports.
   { networking.firewall.allowedTCPPorts = [ 18232 18233 ];
@@ -13,6 +18,24 @@
       home = "/var/lib/zcashd";
       description = "Runs a full Zcash node";
     };
+
+    /*
+     * Nix-generated Tor configuration file has a `User tor` that we cannot
+     * easily control.  This option sets the UID *and* GID of the Tor process.
+     * The keys we want to pass to Tor for the website configuration are in
+     * /run/keys which is group-readable and group-owned by `keys`.  So, get
+     * the tor user's primary group to be the keys group so it can actually
+     * read the keys.
+     *
+     * We have to force this value to override the previously supplied value.
+     *
+     * Note that this means the tor user no longer belongs to the tor group.
+     * This may break something but if it does I haven't yet observed it.
+     *
+     * This is kind of lame, I think.  Maybe it would be better not to have
+     * keys on the disk or something?
+     */
+    users.users.tor.group = lib.mkForce "keys";
 
     environment.systemPackages = [
       zcash
@@ -68,10 +91,109 @@
       };
     };
 
+    /*
+     * Run a Tor node so we can operate a hidden service to allow user signup.
+     */
     services.tor.enable = true;
     /*
      * We don't make outgoing Tor connections via the SOCKS proxy.
      */
     services.tor.client.socksPolicy = "reject *";
+
+    /*
+     * Enable the control port so that we can do interesting things with the
+     * daemon from other programs.
+     */
+    services.tor.controlPort = torControlPort;
+
+    services.tor.extraConfig = ''
+      # Enable authentication on the ControlPort via a secret shared cookie.
+      CookieAuthentication 1
+    '';
+
+    /*
+     * Customize the Tor service so that it is able to read the keys with which
+     * we will supply it.
+     */
+    systemd.services.tor =
+    { serviceConfig =
+      { ReadWritePaths =
+        [ "/var/lib/tor"
+        ];
+      };
+    };
+
+    /* Provide a private key for the website Onion service. */
+    /* https://elvishjerricco.github.io/2018/06/24/secure-declarative-key-management.html */
+    /* https://nixos.org/nixops/manual/#idm140737318276736 */
+    deployment.keys."signup-website-tor-onion-service-v3.secret" =
+    { keyFile = ./secrets/onion-services/v3/signup-website.secret;
+      user = "tor";
+      group = "tor";
+      permissions = "0600";
+    };
+    deployment.keys."signup-website-tor-onion-service-v3.public" =
+    { keyFile = ./secrets/onion-services/v3/signup-website.public;
+      user = "tor";
+      group = "tor";
+      permissions = "0600";
+    };
+    deployment.keys."signup-website-tor-onion-service-v3.hostname" =
+    { keyFile = ./secrets/onion-services/v3/hostname;
+      user = "tor";
+      group = "tor";
+      permissions = "0600";
+    };
+
+    deployment.keys."signup-website-tor-onion-service-v2.private_key" =
+    { keyFile = ./secrets/onion-services/v2/signup-website.private_key;
+      user = "tor";
+      group = "tor";
+      permissions = "0600";
+    };
+    deployment.keys."signup-website-tor-onion-service-v2.hostname" =
+    { keyFile = ./secrets/onion-services/v2/hostname;
+      user = "tor";
+      group = "tor";
+      permissions = "0600";
+    };
+
+    # https://nixos.org/nixos/manual/options.html#opt-systemd.tmpfiles.rules
+    systemd.tmpfiles.rules =
+    [ "d  ${websiteOnion3Dir}                       0700 tor tor - -"
+      "L+ ${websiteOnion3Dir}/hs_ed25519_secret_key -    -   -   - /run/keys/signup-website-tor-onion-service-v3.secret"
+      "L+ ${websiteOnion3Dir}/hs_ed25519_public_key -    -   -   - /run/keys/signup-website-tor-onion-service-v3.public"
+      "L+ ${websiteOnion3Dir}/hostname              -    -   -   - /run/keys/signup-website-tor-onion-service-v3.hostname"
+
+      "d  /run/onion                                0700 tor tor - -"
+      "d  /run/onion/v2                             0700 tor tor - -"
+      "d  ${websiteOnion2Dir}                       0700 tor tor - -"
+      "L+ ${websiteOnion2Dir}/private_key           -    -   -   - /run/keys/signup-website-tor-onion-service-v2.private_key"
+      "L+ ${websiteOnion2Dir}/hostname              -    -   -   - /run/keys/signup-website-tor-onion-service-v2.hostname"
+    ];
+
+    /*
+     * Operate a static website allowing user signup, exposed via the Tor
+     * hidden service.
+     */
+    systemd.services."signup-website" =
+    { unitConfig.Documentation = "https://leastauthority.com/";
+      description = "The S4 2.0 signup website.";
+
+      path = [ (pkgs.python27.withPackages (ps: [ ps.twisted ps.txtorcon ])) ];
+
+      # Get it to start as a part of the normal boot process.
+      wantedBy    = [ "multi-user.target" ];
+
+      # Make sure Tor is up and our keys are available.
+      after = [ "tor.service" "signup-website-tor-onion-service.secret-key.service" ];
+      wants = [ "tor.service" "signup-website-tor-onion-service.secret-key.service" ];
+
+      script = ''
+      twist --log-format=text web \
+        --path ${s4signupwebsite} \
+        --port onion:version=3:public_port=80:controlPort=${toString torControlPort}:hiddenServiceDir=${websiteOnion3Dir}
+      '';
+  };
   };
 }
