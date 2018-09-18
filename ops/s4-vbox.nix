d258daae8b9631dd8d7ac28349d078b8f4a6c6d4
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2018-09-18 14:33:51 -0400

    don't pop up junk

--- ops/s4-vbox.nix
+++ ops/s4-vbox.nix
@@ -4,5 +4,6 @@
     { deployment.targetEnv = "virtualbox";
       deployment.virtualbox.memorySize = 8192; # megabytes
       deployment.virtualbox.vcpu = 1; # number of cpus
+      deployment.virtualbox.headless = true;
     };
 }
