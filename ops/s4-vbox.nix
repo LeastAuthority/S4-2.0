da9cc5dc2ff387a7190d245e24f3167f87edde5e
meejah <meejah@meejah.ca>
2018-09-27 11:33:42 -0600

    use bridged networking

--- ops/s4-vbox.nix
+++ ops/s4-vbox.nix
@@ -5,5 +5,6 @@
       deployment.virtualbox.memorySize = 8192; # megabytes
       deployment.virtualbox.vcpu = 1; # number of cpus
       deployment.virtualbox.headless = true;
+      deployment.virtualbox.vmFlags = ["--nic1" "bridged"];
     };
 }
