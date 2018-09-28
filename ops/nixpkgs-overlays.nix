dcc0ad418e724e186d2470c9d16c74f27eb9c3f9
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2018-09-28 10:41:26 -0400

    Explain why we have a newer Tor version here.

--- ops/nixpkgs-overlays.nix
+++ ops/nixpkgs-overlays.nix
@@ -1,5 +1,11 @@
 self: super:
-{ tor = super.tor.overrideAttrs (old:
+{
+  # Supply a newer version of Tor than is currently available in nixpkgs.
+  # This is necessary to make restarting ephemeral Onion services reliable.
+  # Old versions of Tor sometimes fail to re-upload the descriptor or even if
+  # they succeed in re-uploading it the service remains mysteriously
+  # unavailable.
+  tor = super.tor.overrideAttrs (old:
     rec { name = "tor-0.3.5.2-alpha";
           src = super.fetchurl
           { url = "https://www.torproject.org/dist/tor-0.3.5.2-alpha.tar.gz";
