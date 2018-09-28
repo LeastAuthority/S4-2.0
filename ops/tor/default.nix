8558d97bbcd36a46c7eb6490a37ead26d2b009a8
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2018-09-28 10:41:17 -0400

    The Tor upgrade was inlined in the overlay.

--- ops/tor/default.nix
+++
@@ -1,9 +0,0 @@
-{ tor, fetchurl }:
-tor.overrideAttrs (old:
-rec { name = "tor-0.3.5.2-alpha";
-      src = fetchurl
-      { url = "https://www.torproject.org/dist/tor-0.3.5.2-alpha.tar.gz";
-        sha256 = "16pc99sam4lvgnmvb8sh1k89hskrkl1p79wda93pwdcyry5mvmx4";
-      };
-    }
-)
