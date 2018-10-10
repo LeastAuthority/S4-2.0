661a8a709f0b7cf8242a2de57c119e04610ae206
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2018-10-10 14:33:27 -0400

    Factor zcashd service out

--- ops/s4.nix
+++ ops/s4.nix
@@ -16,6 +16,54 @@ let
 
     inherit script;
   };
+  zcashdService = pkgs: zcash:
+  # Write the Zcashd configuration file and remember where it is for
+  # later.
+  let conf = pkgs.writeText "zcash.conf"
+  ''
+  # Operate on the test network while we're in development.
+  testnet=1
+  addnode=testnet.z.cash
+
+  # Don't be a miner.
+  gen=0
+  '';
+  in
+  { unitConfig.Documentation = "https://z.cash/";
+    description = "Zcashd running a non-mining Zcash full node";
+    # Get it to start as a part of the normal boot process.
+    wantedBy    = [ "multi-user.target" ];
+
+    # Make sure we have network connectivity before bothering to try to
+    # start zcashd.
+    requires = [ "network-online.target" ];
+
+    # Get zcash-fetch-params dependencies into its PATH.
+    path = [ pkgs.utillinux pkgs.wget ];
+
+    serviceConfig = {
+      Restart                 = "on-failure";
+      User                    = "zcash";
+      # Nice                    = 19;
+      # IOSchedulingClass       = "idle";
+      PrivateTmp              = "yes";
+      # PrivateNetwork          = "yes";
+      # NoNewPrivileges         = "yes";
+      # ReadWriteDirectories    = "${zcash}/bin /var/lib/zcashd";
+      # InaccessibleDirectories = "/home";
+      StateDirectory          = "zcashd";
+
+      # Parameters are required before a node can start.  These are fetched
+      # from the network.  This only needs to happen once.  Currently we try
+      # to do it every time we're about to start the node.  Maybe this can
+      # be improved.
+      ExecStartPre            = "${zcash}/bin/zcash-fetch-params";
+
+      # Rely on $HOME to set the location of most Zcashd inputs.  The
+      # configuration file is an exception as it lives in the store.
+      ExecStart               = "${zcash}/bin/zcashd -conf=${conf}";
+    };
+  };
 in
 {
   network.description = "Zcash server";
@@ -70,54 +118,7 @@ in
       pkgs.wget
     ];
 
-    systemd.services.zcashd =
-      # Write the Zcashd configuration file and remember where it is for
-      # later.
-      let conf = pkgs.writeText "zcash.conf"
-      ''
-      # Operate on the test network while we're in development.
-      testnet=1
-      addnode=testnet.z.cash
-
-      # Don't be a miner.
-      gen=0
-    '';
-    in
-    { unitConfig.Documentation = "https://z.cash/";
-      description = "Zcashd running a non-mining Zcash full node";
-      # Get it to start as a part of the normal boot process.
-      wantedBy    = [ "multi-user.target" ];
-
-      # Make sure we have network connectivity before bothering to try to
-      # start zcashd.
-      requires = [ "network-online.target" ];
-
-      # Get zcash-fetch-params dependencies into its PATH.
-      path = [ pkgs.utillinux pkgs.wget ];
-
-      serviceConfig = {
-        Restart                 = "on-failure";
-        User                    = "zcash";
-        # Nice                    = 19;
-        # IOSchedulingClass       = "idle";
-        PrivateTmp              = "yes";
-        # PrivateNetwork          = "yes";
-        # NoNewPrivileges         = "yes";
-        # ReadWriteDirectories    = "${zcash}/bin /var/lib/zcashd";
-        # InaccessibleDirectories = "/home";
-        StateDirectory          = "zcashd";
-
-        # Parameters are required before a node can start.  These are fetched
-        # from the network.  This only needs to happen once.  Currently we try
-        # to do it every time we're about to start the node.  Maybe this can
-        # be improved.
-        ExecStartPre            = "${zcash}/bin/zcash-fetch-params";
-
-        # Rely on $HOME to set the location of most Zcashd inputs.  The
-        # configuration file is an exception as it lives in the store.
-        ExecStart               = "${zcash}/bin/zcashd -conf=${conf}";
-      };
-    };
+    systemd.services.zcashd = zcashdService pkgs zcash;
 
     /*
      * Run a Tor node so we can operate a hidden service to allow user signup.
