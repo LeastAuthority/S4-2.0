7021acce890b1d7608891f23b6cc0cedb5d0ba08
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2018-10-10 10:31:41 -0400

    add lockbox to vbox configuration

--- ops/s4-vbox.nix
+++ ops/s4-vbox.nix
@@ -2,7 +2,17 @@
 { infra =
     { config, pkgs, ... }:
     { deployment.targetEnv = "virtualbox";
-      deployment.virtualbox.memorySize = 8192; # megabytes
+      deployment.virtualbox.memorySize = 4096; # megabytes
+      deployment.virtualbox.vcpu = 1; # number of cpus
+      deployment.virtualbox.headless = true;
+      deployment.virtualbox.vmFlags =
+      [ "--nic1" "bridged" "--bridgeadapter1" bridgeAdapter
+      ];
+    };
+  lockbox =
+    { config, pkgs, ... }:
+    { deployment.targetEnv = "virtualbox";
+      deployment.virtualbox.memorySize = 4096; # megabytes
       deployment.virtualbox.vcpu = 1; # number of cpus
       deployment.virtualbox.headless = true;
       deployment.virtualbox.vmFlags =
