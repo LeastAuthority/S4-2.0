effafb760fc18cd41b449c0255dd63df5151a5bd
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2018-11-01 15:47:07 -0400

    note about future investiation

--- s4/src/S4/Internal/Deployment.hs
+++ s4/src/S4/Internal/Deployment.hs
@@ -7,6 +7,7 @@ import S4.Internal.Wormhole
   )
 
 -- All of the configuration and state of an S4 deployment.
+-- TODO Decide if a GADT is better suited (than this unconstrained polymorphism) for this type
 data Deployment w = Deployment
   { wormholeDelivery :: w
   }
