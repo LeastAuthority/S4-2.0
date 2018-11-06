aefa888c28783dbcded6e133747e59f34a5bd99f
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2018-11-06 13:43:37 -0500

    get the override version of the package to declare its metadata correctly

--- hspec-jenkins-lae/hspec-jenkins-lae.nix
+++ hspec-jenkins-lae/hspec-jenkins-lae.nix
@@ -8,6 +8,7 @@ mkDerivation {
     rev = "2899766bbd36216b81b1719423a5c2443458ae06";
     sha256 = "1w5kcg5n025q4whckyhjgz59i4z4k24isscaawpllfmllc29cjxl";
   };
+  patches = [ ./fix-metadata.patch ];
   libraryHaskellDepends = [ base blaze-markup hspec ];
   homepage = "https://github.com/worksap-ate/hspec-jenkins";
   description = "Jenkins-friendly XML formatter for Hspec";
